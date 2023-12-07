ac77bdeaa60922386a3e4045584129ae819b4e92
MeestersC <chris.meesters@unive.com>
2023-12-07 13:30:54 +0100

    Solved day2b

--- day_2/day_2_cubes.py
+++ day_2/day_2_cubes.py
@@ -23,6 +23,28 @@ def is_game_possible(game: str) -> bool:
     return True
 
 
+def get_pow_highest_colors(game: str) -> int:
+    highest_red = 0
+    highest_green = 0
+    highest_blue = 0
+    for move in game.split(";"):
+        for color in move.split(","):
+            if "red" in color:
+                red = int("".join(filter(str.isdigit, color)))
+                if red > highest_red:
+                    highest_red = red
+            elif "green" in color:
+                green = int("".join(filter(str.isdigit, color)))
+                if green > highest_green:
+                    highest_green = green
+            elif "blue" in color:
+                blue = int("".join(filter(str.isdigit, color)))
+                if blue > highest_blue:
+                    highest_blue = blue
+
+    return highest_red * highest_green * highest_blue
+
+
 def check_games():
     game_i = 1
     for game in games:
@@ -32,5 +54,14 @@ def check_games():
         game_i = game_i + 1
 
 
+def sum_games() -> int:
+    highest_colors: list[int] = []
+    for game in games:
+        game = game[game.find(":") + 1 :]
+        highest_colors.append(get_pow_highest_colors(game))
+    return sum(highest_colors)
+
+
 check_games()
 print(sum(possible_games))
+print(sum_games())
