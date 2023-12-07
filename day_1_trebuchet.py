43ad05f22aaaa9c58cf29c3c8bec3c9362d0bcce
MeestersC <chris.meesters@unive.com>
2023-12-07 12:10:54 +0100

    Added support for solution 1a and 1b

--- day_1_trebuchet.py
+++ day_1_trebuchet.py
@@ -37,15 +37,18 @@ def concat_numbers(first: str, last: str) -> int:
     return int(str(first) + str(last))
 
 
-def calculate_calibration_data(filename: str) -> int:
+def calculate_calibration_num(filename: str, contains_num_str: bool) -> int:
     file = open(filename, "r")
     lines = file.readlines()
     total: int = 0
     for word in lines:
-        word = replace_numbers_in_str(word)
+        if contains_num_str:
+            word = replace_numbers_in_str(word)
         total = total + (concat_numbers(get_first_int(word), get_last_int(word)))
 
     return total
 
 
-print(calculate_calibration_data("calibration_data.txt"))
+print(
+    f"The calibration number is: {calculate_calibration_num('calibration_data.txt', True)}"
+)
